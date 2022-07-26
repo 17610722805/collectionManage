2bd0c99422a4bffbdb8c9a930c4851ada7a38322
weiyuqiang <17610722805@163.com>
2022-07-26 18:44:44 +0800

    初始化

--- ruoyi-generator/src/main/java/com/ruoyi/generator/util/GenUtils.java
+++ ruoyi-generator/src/main/java/com/ruoyi/generator/util/GenUtils.java
@@ -1,6 +1,9 @@
 package com.ruoyi.generator.util;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.lang3.RegExUtils;
 import com.ruoyi.common.constant.GenConstants;
 import com.ruoyi.common.utils.StringUtils;
@@ -152,15 +155,41 @@ public class GenUtils
 
     /**
      * 获取业务名
-     * 
+     *
      * @param tableName 表名
      * @return 业务名
      */
-    public static String getBusinessName(String tableName)
-    {
-        int lastIndex = tableName.lastIndexOf("_");
-        int nameLength = tableName.length();
-        return StringUtils.substring(tableName, lastIndex + 1, nameLength);
+    public static String getBusinessName(String tableName) {
+        return underlineToHump(tableName);
+//        int lastIndex = tableName.lastIndexOf("_");
+//        int nameLength = tableName.length();
+//        return StringUtils.substring(tableName, lastIndex + 1, nameLength);
+    }
+
+    /**
+     * 根据传入的带下划线的字符串转化为驼峰格式
+     *
+     * @param str
+     * @return
+     * @author mrf
+     */
+
+    public static String underlineToHump(String str) {
+        //正则匹配下划线及后一个字符，删除下划线并将匹配的字符转成大写
+        Matcher matcher = Pattern.compile("_([a-z])").matcher(str);
+        StringBuffer sb = new StringBuffer(str);
+        if (matcher.find()) {
+            sb = new StringBuffer();
+            //将当前匹配的子串替换成指定字符串，并且将替换后的子串及之前到上次匹配的子串之后的字符串添加到StringBuffer对象中
+            //正则之前的字符和被替换的字符
+            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+            //把之后的字符串也添加到StringBuffer对象中
+            matcher.appendTail(sb);
+        } else {
+            //去除除字母之外的前面带的下划线
+            return sb.toString().replaceAll("_", "");
+        }
+        return underlineToHump(sb.toString());
     }
 
     /**
